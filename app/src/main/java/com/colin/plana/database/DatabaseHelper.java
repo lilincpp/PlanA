9ba4e7ac9056d52c0030e7d7a0f8de81510e8caf
木子木木 <lilin@dingdangrobot.com>
2017-09-25 18:06:25 +0800

    创建数据库及相关操作

---
+++ app/src/main/java/com/colin/plana/database/DatabaseHelper.java
@@ -0,0 +1,31 @@
+package com.colin.plana.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by colin on 2017/9/25.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "WeeklyTaskList.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static volatile DatabaseHelper INSTANCE = null;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
